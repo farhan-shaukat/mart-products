9d8b8271aa18b664d39f17092afa19726d5fe3bb
Farhan <farhanshokat@gmail.com>
2024-07-10 17:52:56 +0500

    Project 3 initialize database guide line.

--- app/main.py
+++ app/main.py
@@ -1,24 +1,51 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from .database import create_db_and_tables
-from .router import item  
+from fastapi import Body, FastAPI
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-]
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
 
-@app.on_event("startup")
-async def on_startup():
-    create_db_and_tables()
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+        
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
 
-app.include_router(item.router) 
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
